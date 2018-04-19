af4fcb092e2aa66e6bf29e1856be027ef8be0af3
Tan-Moy <tanmoy.pto@gmail.com>
2018-04-19 13:37:20 +0530

    added favorite functionality

--- index.js
+++ index.js
@@ -17,12 +17,50 @@ const retweet = (q) =>{
 	T.get('search/tweets', params, function(err, data) {
   		if(!err){
   			//grab id
-  			let usrId = data.statuses[0].id_str;
+  			let usrId = data.statuses.length < 10 ? data.statuses[0].id_str : data.statuses[Math.floor(Math.random() * 10)].id_str;
+  			// console.log(usrId)
 
-  			//
-  			console.log(usrId);
+  			//retweet
+  			T.post('statuses/retweet/:id', { id: usrId}, function (err, data, response) {
+  				if(response){console.log('retweet successfull')};
+  				if(err){console.log(err)};
+			})
   		}
 	})
 }
 
-retweet('#100DaysOfCode');
+//Favorite
+const favorite = (q) =>{
+	const params = {
+		q: q,
+		result_type:'recent',
+		lang:'en',
+		count:'10'
+	}
+
+	T.get('search/tweets', params, function(err, data) {
+  		if(!err){
+  			//grab id
+  			let usrId = data.statuses.length < 10 ? data.statuses[0].id_str : data.statuses[Math.floor(Math.random() * 10)].id_str;
+  			// console.log(usrId)
+
+  			//like
+  			T.post('favorites/create', { id: usrId}, function (err, data, response) {
+  				if(response){console.log('Favorited successfully')};
+  				if(err){console.log(err)};
+			})
+  		}
+	})
+}
+
+//Follow
+
+
+
+
+
+//call all functions
+// retweet('#100DaysOfCode');
+favorite('#IOT');
+
+//choose a random topic and follow,retweet and like it in set intervals
